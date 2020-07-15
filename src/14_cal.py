5120ab87699c651bc11ea187202d9d0e0f76e300
Brennan Neilson <bvneilson@gmail.com>
2020-07-14 21:34:49 -0600

    second half complete

--- src/14_cal.py
+++ src/14_cal.py
@@ -11,6 +11,7 @@ and does the following:
    module may be helpful for this.
  - If the user specifies one argument, assume they passed in a
    month and render the calendar for that month of the current year.
+
  - If the user specifies two arguments, assume they passed in
    both the month and the year. Render the calendar for that
    month and year.
@@ -18,15 +19,27 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
+Note: the user should provide argument input (in the initial call to run the file) and not
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you would call `python3 14_cal.py 4 2015` to
+print out a calendar for April in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
 import sys
 import calendar
 from datetime import datetime
+
+my_cal = calendar.TextCalendar()
+month = datetime.now().month
+year = datetime.now().year
+arg_len = len(sys.argv)
+
+if arg_len == 1:
+    print(my_cal.formatmonth(year, month))
+elif arg_len == 2:
+    print(my_cal.formatmonth(year, int(sys.argv[1])))
+elif arg_len == 3:
+    print(my_cal.formatmonth(int(sys.argv[2]), int(sys.argv[1])))
